a0adf67e110ea76a6bded3f8d42bb96bf0c52176
megagus182 <megagus182@hotmail.com>
2022-12-12 10:15:01 -0600

    responsive

--- src/components/Contactame.jsx
+++ src/components/Contactame.jsx
@@ -19,6 +19,7 @@ import tik from "../images/tik.png";
 import insta from "../images/insta.png";
 import call from "../images/llamame.png";
 import "./Home.css";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function Contactame() {
   const [input, setInput] = useState("");
@@ -31,6 +32,7 @@ export default function Contactame() {
   const isNameError = inputName === "";
   const isMessageError = inputMessage === "";
   const form = useRef();
+  const celular = useMediaQuery('(min-width:450px)');
 
   function sendEmail(e) {
     e.preventDefault();
@@ -49,8 +51,11 @@ export default function Contactame() {
   }
 
   return (
+    <Box>
+    {celular ?
     <Box className="hommie" marginTop={0} padding="20px">
-        <Paper sx={{margin:"20px", padding:"20px", backgroundColor: "rgb(252 228 236 / 50%)",marginTop:0}} elevation={5}>
+      
+      <Paper sx={{margin:"20px", padding:"20px", backgroundColor: "rgb(252 228 236 / 50%)",marginTop:0}} elevation={5}>
       <Box display={"flex"} alignItems="center" textAlign={"center"} justifyContent={"center"}>
         <Box>
         <Typography textAlign={"center"} variant="h3">
@@ -193,7 +198,162 @@ export default function Contactame() {
           </Box>
         </Box>
       </Box>
-      </Paper>
+      </Paper> 
+      </Box>
+      : 
+      <Box className="hommie"  padding="5px">
+      <Paper sx={{margin:"10px", backgroundColor: "rgb(252 228 236 / 50%)"}} elevation={10}>
+      <Box display={"flex"} className="hommie" flexDirection={"column"} alignItems="center" textAlign={"center"} justifyContent={"center"}>
+        <Box>
+        <Typography textAlign={"center"} paddingTop={"10px"} variant="h5">
+            ¿Necesitas una Ilustración Diseño o logotipo?
+          </Typography>
+          <Typography textAlign={"center"} variant="h5" paddingTop={"10px"}>
+            ¡Trabajemos juntos!
+          </Typography>
+        </Box>
+        <Box display={"contents"}>
+          <img src={call} width="80%" alt="llamame" />
+        </Box>
+      </Box>
+      <Box className="hommie" margin={0}>
+      <Typography
+        textAlign={"center"}
+        variant="h4"
+        paddingTop={"10px"}
+      >
+        Contactame!
+      </Typography>
+      <Box width={"100%"} display={"flex"} flexDirection={"column-reverse"} className="Home">
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          width={"100%"}
+          textAlign={"center"}
+          className="leftSideFromHome"
+        >
+          {/* ICONOS */}
+          <Typography
+        textAlign={"center"}
+        variant="h5"
+        paddingTop={"10px"}
+      >
+        Sigueme en mis redes sociales!
+      </Typography>
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+          >
+            
+            <Box margin={"10px"}>
+              <Tooltip label="Go to my Facebook" aria-label="A tooltip">
+                <a href="https://www.facebook.com/Art.giss">
+                  <img
+                    height={"fit-content"}
+                    width={"50%"}
+                    src={face}
+                    alt="github"
+                  />
+                </a>
+              </Tooltip>
+              
+            </Box>
+            <Box>
+              <Tooltip label="Go to my Instagram" aria-label="A tooltip">
+                <a href="https://www.instagram.com/art.giss.x3/">
+                  <img
+                    height={"fit-content"}
+                    width={"50%"}
+                    src={insta}
+                    alt="linkedin"
+                  />
+                </a>
+              </Tooltip>
+              
+            </Box>
+            <Box>
+              <Tooltip label="Go to my TikTok" aria-label="A tooltip">
+                <a
+                  href="https://www.tiktok.com/@gissx3"
+                >
+                  <img
+                    height={"fit-content"}
+                    width={"50%"}
+                    src={tik}
+                    alt="cv"
+                  />
+                </a>
+              </Tooltip>
+              
+            </Box>
+          </Box>
+        </Box>
+        {/* FORMULARIO */}
+        <Box>
+          <Box>
+            <form ref={form} onSubmit={sendEmail}>
+              <FormGroup style={{margin:"15px"}} >
+                <FormLabel>Nombre</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Name"
+                  name="from_name"
+                  value={inputName}
+                  onChange={handleInputNameChange}
+                />
+                {!isNameError ? (
+                  <FormHelperText>Ingresa tu nombre</FormHelperText>
+                ) : (
+                  <FormHelperText>Nombre es requerido</FormHelperText>
+                )}
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  value={input}
+                  onChange={handleInputChange}
+                />
+                {!isEmailError ? (
+                  <FormHelperText>
+                    Ingresa el email donde quieres recibir mi respuesta
+                  </FormHelperText>
+                ) : (
+                  <FormHelperText>Email es requerido</FormHelperText>
+                )}
+                <FormLabel>Mensaje</FormLabel>
+                <TextField
+                  type="text"
+                  height={"20"}
+                  name="message"
+                  placeholder="Message"
+                  value={inputMessage}
+                  onChange={handleInputMessageChange}
+                />
+                {!isMessageError ? (
+                  <FormHelperText>
+                    Escribe un mensaje para contactarme
+                  </FormHelperText>
+                ) : (
+                  <FormHelperText>Mensaje es requerido.</FormHelperText>
+                )}
+                <br></br>
+                {!isEmailError && !isNameError && !isMessageError ? (
+                  <Button variant="contained" type="submit">Enviar</Button>
+                ) : null}
+              </FormGroup >
+            </form>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+    </Paper>
+      </Box>
+      }
+        
     </Box>
   );
 }
--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,12 +1,16 @@
-import { Paper, Typography, ImageList, ImageListItem } from "@mui/material";
+import { Paper, Typography, ImageList, ImageListItem, Box } from "@mui/material";
 import 'animate.css';
 import "./Galeria.css";
 import { itemData } from "./imagenes";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
 export default function Galeria(){
+  const celular = useMediaQuery('(min-width:450px)');
     return(
-      <div className="hommie" style={{paddingBottom:"1px", paddingTop:"1px"}} >
+      <Box>
+        {celular ?  
+        <div className="hommie" style={{paddingBottom:"1px", paddingTop:"1px"}} >
         <Paper
         sx={{
           backgroundColor: "#fce4ec",
@@ -47,5 +51,50 @@ export default function Galeria(){
         </ImageList>
       </Paper>
        </div>
+        : 
+        <div className="hommie" style={{paddingBottom:"1px", paddingTop:"1px"}} >
+        <Paper
+        sx={{
+          backgroundColor: "#fce4ec",
+          margin: "10px",
+          padding: "10px",
+          textAlign: "center",
+          borderRadius: "20px",
+        }}
+        elevation={5}
+      >
+        <Typography variant="h2" color={"#9575cd"}>
+          Mis obras
+        </Typography>
+        <ImageList sx={{ margin: 2 }} variant="masonry" cols={1} gap={5}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <div class="wrapper">
+              <img
+              className="imgHome"
+                style={{
+                  borderRadius: 10,
+                  border: "solid",
+                  borderWidth: "thin",
+                }}
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+               <div class="overlay">
+                  <div class="content">
+                    {item.title} 
+                  </div>
+                </div>
+                </div>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Paper>
+       </div>
+       }
+      
+       </Box>
     )
 }
